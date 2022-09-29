3b711b0d6af0fd2e40a21bc6ad98dc44145d2ff5
THAI <hoangthai2792000@gmail.com>
2022-09-29 12:55:16 +0700

    change message landing page

--- client/src/pages/Landing.js
+++ client/src/pages/Landing.js
@@ -1,7 +1,7 @@
-import main from '../assets/images/main.svg';
-import Wrapper from '../assets/wrappers/LandingPage';
-import { Logo } from '../components';
-import { Link } from 'react-router-dom';
+import main from '../assets/images/main.svg'
+import Wrapper from '../assets/wrappers/LandingPage'
+import { Logo } from '../components'
+import { Link } from 'react-router-dom'
 const Landing = () => {
   return (
     <Wrapper>
@@ -14,11 +14,7 @@ const Landing = () => {
           <h1>
             job <span>tracking</span> app
           </h1>
-          <p>
-            Crucifix narwhal street art asymmetrical, humblebrag tote bag pop-up
-            fixie raclette taxidermy craft beer. Brunch bitters synth, VHS
-            crucifix heirloom meggings bicycle rights.
-          </p>
+          <p>Đụ mẹ mày</p>
           <Link to='/register' className='btn btn-hero'>
             Login/Register
           </Link>
@@ -26,7 +22,7 @@ const Landing = () => {
         <img src={main} alt='job hunt' className='img main-img' />
       </div>
     </Wrapper>
-  );
-};
+  )
+}
 
-export default Landing;
+export default Landing
